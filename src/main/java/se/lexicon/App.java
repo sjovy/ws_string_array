e123c7fc7fca51e956af31d1dffb8061b47b3379
sjovy <thomas.sjovy@gmail.com>
2024-03-13 13:35:20 +0100

    Added second metjhod to add name to array Also tightened code considerably.

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,32 +1,66 @@
 package se.lexicon;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     static String[] names = {"Erik Svensson"};
 
     public static void main( String[] args ) {
-        //String[] names = new String[1];
-        //names[0] = "Erik Svensson";
-        //System.out.println(names[0]);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter name to search: ");
-        String name = scanner.nextLine();
-        find(name);
+
+        int action;
+        String name;
+        Scanner scanner;
+
+        System.out.println("Do you want to... ");
+        System.out.println("search for name, press 1. ");
+        System.out.println("add new name, press 2. ");
+
+        scanner = new Scanner(System.in);
+        action = scanner.nextInt();
+        System.out.println("Enter name: ");
+        scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+
+        switch (action) {
+            case 1:
+                findName(name);
+                break;
+            case 2:
+                addName(name);
+                break;
+            default:
+                System.out.println("Invalid input");
+                System.exit(0);
+        }
+        System.out.println(names.length);
+        System.out.println("Thank You");
     }
 
-    public static String find(final String fullName) {
-        String nameFound = "";
-        for (int i = 0; i < names.length; i++) {
-            if (names[i].equalsIgnoreCase(fullName)) {
-                nameFound = names[i];
-                System.out.println(nameFound);
-            } else {
-                nameFound = null;
-                System.out.println("Name not found");
+    public static String findName(final String fullName) {
+        for (String name : names) {
+            if (name.equalsIgnoreCase(fullName)) {
+                System.out.println(fullName);
+                return fullName;
             }
         }
-        return nameFound;
+        System.out.println("null");
+        return null;
     }
+
+    public static boolean addName(final String fullName) {
+        for (String name : names) {
+            if (name.equalsIgnoreCase(fullName)) {
+                System.out.println("Name already exists");
+                return false;
+            }
+        }
+        String[] tempNames = Arrays.copyOf(names, names.length + 1);
+        tempNames[tempNames.length - 1] = fullName;
+        names = tempNames;
+        System.out.println("Name added");
+        return true;
+    }
+
 }
