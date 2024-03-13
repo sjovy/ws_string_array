a72ad55eed2590b25ab0271ad56e9b1f43494f62
sjovy <thomas.sjovy@gmail.com>
2024-03-13 10:46:46 +0100

    Initialize array with one name. Method to ask for name to search and method to look for and return that name. Else returns null

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,32 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.Scanner;
+
+public class App {
+    static String[] names = {"Erik Svensson"};
+
+    public static void main( String[] args ) {
+        //String[] names = new String[1];
+        //names[0] = "Erik Svensson";
+        //System.out.println(names[0]);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name to search: ");
+        String name = scanner.nextLine();
+        find(name);
+    }
+
+    public static String find(final String fullName) {
+        String nameFound = "";
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equalsIgnoreCase(fullName)) {
+                nameFound = names[i];
+                System.out.println(nameFound);
+            } else {
+                nameFound = null;
+                System.out.println("Name not found");
+            }
+        }
+        return nameFound;
     }
 }
